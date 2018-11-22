cc922ee17164356fd649e9c17a9dcc7b675a30c0
hassehulabeck <hansbudandersson@gmail.com>
2018-11-22 12:05:49 +0100

    First commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <p> {{ counter }} </p>
+    <button @click="increase(1)">+1</button>
+    <button @click="decrease(1)">-1</button>
+    <button @click="increase(10)">+10</button>
+    <button @click="decrease(10)">-10</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Counter',
+  computed: {
+    counter() {
+      return this.$store.state.counter
+    }
+  },
+  methods: {
+    increase(antal) {
+      this.$store.commit('inc', antal)
+    },
+    decrease(antal) {
+      this.$store.commit('dec', antal)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -1,14 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+// Make sure to call Vue.use(Vuex) first if using a module system
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    counter: 0
   },
   mutations: {
-
+    inc(state, antal) {
+      state.counter += antal
+    },
+    dec: (state, antal) => state.counter -= antal
   },
   actions: {
 
